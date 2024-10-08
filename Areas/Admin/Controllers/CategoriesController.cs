f011089e219d5d7cf8d1b4544494e618580bfe30
SV <SV@PM19-PC27>
2024-10-08 11:29:18 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH114365_MyStore.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Admin/Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
